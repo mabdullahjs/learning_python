c37ee92bbbebdf805db5ecac7ec0a2ddcb932cbb
mabdullahjs <mabdullah2037@outlook.com>
2025-05-06 10:36:57 +0500

    basic data types

---
+++ 02-Data-Types/datatypes.py
@@ -0,0 +1,62 @@
+# print("hello world")
+
+
+# numbers
+
+
+# import random
+# import math
+
+# age = 21
+
+# print(age)
+# print(random.random())
+
+
+# print (2 ** 500)
+
+# print(random.choice([1, 5, 6, 7, 8, 9]))
+# print(math.pi)
+
+
+# strings
+
+
+# username = "Abdullah"
+# print(len(username))
+# print(username[2])
+# print(username[-2])
+# print(username[1:5])
+
+# print(dir(username))
+
+# username[1] = "C" string is ummutable
+
+
+# list
+
+
+# fruits = ['apple' , 'banana' , 'orange' , 'grapes']
+# print(fruits)
+# print(len(fruits))
+# print(fruits[1])
+# print(fruits[1 : 3])
+
+
+# Dictionaries
+
+# user = {
+#     "name": "Muhammad Abdullah",
+#     "email": "mabdullah2037@gmail.com",
+#     "age": 21,
+#     "isLoggedIn": True,
+#     "hobbies": ['swimming' , 'eating' , 'sleeping']
+# }
+
+# print(user['hobbies'][1])
+
+
+# Tuples
+
+# myTup = (1, 2, 3)
+# print(myTup)
