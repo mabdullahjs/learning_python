c773b6e33160feb3ac72951586897b363baa62c5
mabdullahjs <mabdullah2037@outlook.com>
2025-05-11 19:33:11 +0500

    numbers

--- 03-Numbers/number.py
+++ 03-Numbers/number.py
@@ -1,3 +1,68 @@
-print('hello numbers')
-repr('hello')
-str('hello')
+# print('hello numbers')
+
+# num1 = 20
+# num2 = 300;
+
+# print(num1 ** num2)
+
+# print(int("10"))
+# print(float("3.14"))
+# print(str(100))
+# print(bool(0))
+# print(bool(1))
+
+
+# import math
+
+# print(math.ceil(20.6))
+# print(math.floor(20.6))
+# print(math.factorial(5))
+# print(5*4*3*2*1)
+# print(math.pi)
+
+
+import random
+
+# print(random.random())
+# print(random.randint(1, 10))
+# print(random.choice([1, 4, 7, 8]))
+# mylist = ['mango' , 'orange' , 'banana']
+# random.shuffle(mylist)
+# print(mylist)
+
+
+
+
+
+
+
+
+
+
+# print(0.1 + 0.2)
+
+# from decimal import Decimal
+# a = Decimal('0.1')
+# b = Decimal('0.2')
+
+# print(a + b)
+
+
+
+
+
+
+
+# x = 10
+# y = 5
+
+# print(x == y)   # Equal
+# print(x != y)   # Not equal
+# print(x > y)    # Greater than
+# print(x >= y)   # Greater than or equal
+# print(x < y)    # Less than
+# print(x <= y)   # Less than or equal
+
+# print(x and y)  # Logical AND
+# print(x or y)   # Logical OR
+# print(not x)    # Logical NOT
