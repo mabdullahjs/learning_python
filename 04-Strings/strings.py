4db1e9a8b522270e62919a53be793da431ad86d7
mabdullahjs <mabdullah2037@outlook.com>
2025-05-15 12:12:06 +0500

    simple strings

---
+++ 04-Strings/strings.py
@@ -0,0 +1,34 @@
+# print('hello strings')
+
+
+# username = "abdullah"
+# print(len(username))
+# print("abdss" in username)
+# print(username  * 3)
+
+# print(username[0])
+# print(username[-1])
+# print(username[1 : 4])
+
+
+
+
+
+
+username = "  Abdullah  "
+age = 21
+# msg = f"My name is {username} and I am {age} years old."
+msg = f"My name is abdullah and I am 21 years old."
+fruits = ['apple' , 'banana' , 'orange' , 'mango' , 'watermelon']
+
+# print(msg)
+
+print(username.lower())
+print(username.upper())
+print(username.strip())
+print(username.replace('bd' , 'au'))
+print(msg.split(' '))
+print(" ".join(fruits))
+print(msg.find("abdullah"))
+print(msg.startswith("M"))
+print(msg.endswith("."))
