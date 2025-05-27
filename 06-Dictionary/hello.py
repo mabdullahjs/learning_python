f918dab9814d099621f9e6741f7226e81f275c44
mabdullahjs <mabdullah2037@outlook.com>
2025-05-27 12:21:40 +0500

    dictionaries

---
+++ 06-Dictionary/hello.py
@@ -0,0 +1,74 @@
+# print('hello dictionary')
+
+user = {
+    "fullname": "Muhammad abdullah",
+    "email": "mabdullah2037@gmail.com",
+    "age": 21,
+    "isLoggedIn": True,
+    "hobbies": ['swimming' , 'coding' , 'traveling']
+}
+
+# print(user)
+# updading
+
+# user["fullname"] = "Muhammad"
+# print(user)
+
+# deleting
+
+# del user["hobbies"]
+# user.pop("age")
+# print(user)
+
+# user.clear()
+# print(user)
+
+
+
+
+
+
+# loops
+
+    # print('f{keys}' + "=>" + user[keys])
+# for keys in user:
+#     print(f'{keys} => {user[keys]}')
+
+
+
+
+
+
+# methods
+# print(user.keys())
+# print(user.values())
+# print(user.items())
+
+
+
+
+
+
+
+# nested_dict = {
+#     "student1": {
+#         "name": "Ali",
+#         "age": 20,
+#         "grades": [85, 90, 78]
+#     },
+#     "student2": {
+#         "name": "Aisha",
+#         "age": 22,
+#         "grades": [88, 92, 81]
+#     }
+# }
+
+# print(nested_dict["student1"]["name"])
+
+
+
+
+
+
+# square = {x: x**2 for x in range(6)}
+# print(square)
