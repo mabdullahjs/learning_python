b31dccec9b1871377808d6e01907c18008d5dbb8
mabdullahjs <mabdullah2037@outlook.com>
2025-05-27 14:33:18 +0500

    tuples

---
+++ 07-Tuples/hello.py
@@ -0,0 +1,12 @@
+# print('tuples')
+
+# list and dic is mutable
+
+# tuple is ummutable
+
+# my_tuple = (1, 2, 3, "hello", True)
+# print(my_tuple)
+
+
+# print(my_tuple[-1])
+# print(len(my_tuple))
